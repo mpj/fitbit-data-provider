d5c693613438a798895817401fc56c63ae04f967
Mattias Petter Johansson <mpj@spotify.com>
2013-03-03 09:01:00 +0100

    Refactoring

--- provider.js
+++ provider.js
@@ -8,35 +8,45 @@ FitbitStepsProvider.getInstance = function() {
 }
 
 FitbitStepsProvider.prototype.getSteps = function(callback) {
+  getTimeSeriesMax('activities/steps', callback)
+}
+
+function getTimeSeriesMax(resourcePath, callback) {
 
   var credentialsProvider = FitbitCredentialsProvider.getInstance()
   var credentials = credentialsProvider.get()
-
   var client = fitbit(credentials.apiKey, credentials.apiSecret)
-  client.apiCall(
-    'GET',
-    '/user/-/activities/steps/date/today/max.json',
-    {
-      token: {
-        oauth_token: credentials.token,
-        oauth_token_secret: credentials.tokenSecret
-      }
-    }, function(error, resp, data) {
-      var steps, map;
 
-      if (error) {
-        callback({
-          message: 'Error retrieving data from Fitbit. See innerError property for more info.',
-          innerError: error
-        }, null);
-        return;
-      }
+  var method = 'GET'
+  var url = '/user/-/' + resourcePath + '/date/today/max.json'
+  var params = {
+    token: {
+      oauth_token: credentials.token,
+      oauth_token_secret: credentials.tokenSecret
+    }
+  }
 
-      steps = data['activities-steps']
-      map = {}
-      steps.forEach(function(s) { map[s.dateTime] = s.value })
-      callback(null, map);
-    })
+  client.apiCall(method, url, params, function(error, resp, data) {
+    var points, property, map
+
+    if (error) {
+      callback({
+        message: 'Error retrieving data from Fitbit. See innerError property for more info.',
+        innerError: error
+      }, null)
+      return
+    }
+
+    // Example: If the resource path is 'activities/steps',
+    // the name of the property will be 'activities-steps'
+    propertyName = resourcePath.replace('/', '-')
+    points = data[propertyName]
+
+    map = {}
+    points.forEach(function(p) { map[p.dateTime] = p.value })
+    callback(null, map)
+  })
+
 }
 
 module.exports = FitbitStepsProvider
